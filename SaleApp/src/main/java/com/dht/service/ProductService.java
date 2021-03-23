135734762695b3f5839bdad1bb9f89d4b1803eac
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2021-03-23 16:41:22 +0700

    update

--- SaleApp/src/main/java/com/dht/service/ProductService.java
+++ SaleApp/src/main/java/com/dht/service/ProductService.java
@@ -13,28 +13,32 @@ import java.sql.SQLDataException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Admin
  */
 public class ProductService {
+
     private Connection conn;
-    
+
     public ProductService(Connection conn) {
         this.conn = conn;
     }
-    
+
     public List<Product> getProducts(String kw) throws SQLException {
-        if (kw == null)
+        if (kw == null) {
             throw new SQLDataException();
-        
+        }
+
         String sql = "SELECT * FROM product WHERE name like concat('%', ?, '%')";
         PreparedStatement stm = this.conn.prepareStatement(sql);
         stm.setString(1, kw);
-        
+
         ResultSet rs = stm.executeQuery();
-        
+
         List<Product> products = new ArrayList<>();
         while (rs.next()) {
             Product p = new Product();
@@ -43,9 +47,27 @@ public class ProductService {
             p.setDescription(rs.getString("description"));
             p.setPrice(rs.getBigDecimal("price"));
             p.setCategoryId(rs.getInt("category_id"));
-            
+
             products.add(p);
         }
         return products;
+    }
+
+    public boolean addProduct(Product p) {
+        try {
+            String sql = "INSERT INTO product(name, price, category_id) VALUES(?, ?, ?)";
+            PreparedStatement stm = this.conn.prepareStatement(sql);
+            stm.setString(1, p.getName());
+            stm.setBigDecimal(2, p.getPrice());
+            stm.setInt(3, p.getCategoryId());
+
+            int rows = stm.executeUpdate();
+
+            return rows > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
     }
 }
--- SaleApp/src/test/java/com/dht/service/ProductTester.java
+++ SaleApp/src/test/java/com/dht/service/ProductTester.java
@@ -6,6 +6,7 @@
 package com.dht.service;
 
 import com.dht.pojo.Product;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLDataException;
 import java.sql.SQLException;
@@ -16,7 +17,11 @@ import java.util.logging.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  *
@@ -59,6 +64,7 @@ public class ProductTester {
     }
     
     @Test
+    @Tag("critical")
     public void testWithUnknownKeyWord() {
         try {
             ProductService s = new ProductService(conn);
@@ -84,5 +90,59 @@ public class ProductTester {
             ProductService s = new ProductService(conn);
             List<Product> products = s.getProducts("");
         });
+    }
+    
+    @Test
+    @DisplayName("Kiem thu them sp voi name = null")
+    @Tag("critical")
+    public void tesAddProductWithNameNull() throws SQLException {
+        ProductService s = new ProductService(conn);
+        Product p = new Product();
+        p.setName(null);
+        p.setPrice(new BigDecimal(999));
+        p.setCategoryId(1);
+        
+        Assertions.assertFalse(s.addProduct(p));
+    }
+    
+    @Test
+    @Tag("critical")
+    public void tesAddProductWithInvalidCate() throws SQLException {
+        ProductService s = new ProductService(conn);
+        Product p = new Product();
+        p.setName("TEST PHONE");
+        p.setPrice(new BigDecimal(999));
+        p.setCategoryId(9999);
+        
+        Assertions.assertFalse(s.addProduct(p));
+    }
+    
+    @Test
+    @Tag("critical")
+    public void tesAddProduct() throws SQLException {
+        ProductService s = new ProductService(conn);
+        Product p = new Product();
+        p.setName("TEST PHONE");
+        p.setPrice(new BigDecimal(999));
+        p.setCategoryId(1);
+        
+        Assertions.assertTrue(s.addProduct(p));
+        
+        //
+    }
+    
+    @ParameterizedTest
+    @CsvSource({"P1,999,1,true", "P2,999,99,false", ",99,1,false"})
+    public void tesAddProductParams(String name, BigDecimal price, int cateId, boolean expected) throws SQLException {
+        ProductService s = new ProductService(conn);
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        p.setCategoryId(cateId);
+        
+        Assertions.assertEquals(expected, s.addProduct(p));
+//        Assertions.assertTrue(s.addProduct(p));
+        
+        //
     }
 }
